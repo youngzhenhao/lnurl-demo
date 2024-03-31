e17e6e3dd435f5133b2ed5a09b17af0effaf3ad0
youngzhenhao <2499342961@qq.com>
2024-03-31 14:22:19 +0800

    update qrc

--- qrc/qrc.js
+++ qrc/qrc.js
@@ -1,9 +1,8 @@
 const args = require('minimist')(process.argv.slice(2))
 const QRCode = require("qrcode");
 async function main(str) {
-    address = str;
     console.log(
-        await QRCode.toString(address, { type: "terminal", small: true })
+        await QRCode.toString(str, { type: "terminal", small: true })
     );
 }
-main(args['lnu']);
+main(args['url']);
